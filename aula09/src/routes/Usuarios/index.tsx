34f6d9bdc7a6ffe99aa50eefed0ed4c26f93c32f
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-05 20:38:24 -0300

    Realizando fetch p api github lista users.

--- aula09/src/routes/Usuarios/index.tsx
+++ aula09/src/routes/Usuarios/index.tsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Usuarios(){
 
     //MUDANDO O TÍTULO DA PÁGINA!!!
     document.title = "Usuarios";
-    const [usuario, setUsuario] = useState({
+    // const [usuario, setUsuario] = useState({
+    //   login: "",
+    //   avatar_url:""
+    // });
+    
+    const [usuarios, setUsuarios] = useState( [ {
       login: "",
       avatar_url:""
-    });
+    }]);
     
+
+
+    useEffect(()=>{
+      
       //Realizando uma requisição para uma API-REST(api do github) com fetch
-      fetch('https://api.github.com/users/alecarlosjesus')
+      fetch('https://api.github.com/users')
       .then(response =>{
           if(!response.ok){
               throw new Error(`Erro: ${response.status}`);
@@ -19,20 +28,21 @@ export default function Usuarios(){
         })
         .then(data => {
           console.log(data);
-          setUsuario(data);
+          setUsuarios(data);
         })
         .catch(error => console.error("Erro ao buscar usuário : ", error));
+    },[]);
       
     return(
       <div>
         <h1>Bem Vindo!</h1>
 
-          <h2>{usuario.login}</h2>
-
-          <figure>
-            <img src={usuario.avatar_url} alt="Imagem do Usuário"/>
-          </figure>
-
+        { usuarios.map((usuario)=>(
+          <div key={usuario.login}>
+            <h2>{usuario.login}</h2>
+            <img src={usuario.avatar_url} alt={usuario.login} />
+          </div>
+        ))}
 
       </div>
     );
