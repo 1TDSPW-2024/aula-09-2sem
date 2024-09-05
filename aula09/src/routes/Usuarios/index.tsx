db223c6f196ccf8d657aa8f5f83cbcb7297d87fb
gustavoamorim12 <guamorim01@gmail.com>
2024-09-05 20:59:17 -0300

    terminei

--- aula09/src/routes/Usuarios/index.tsx
+++ aula09/src/routes/Usuarios/index.tsx
@@ -1,38 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Usuarios(){
 
     //MUDANDO O TÍTULO DA PÁGINA!!!
     document.title = "Usuarios";
-    const [usuario, setUsuario] = useState({
-      login: "",
-      avatar_url:""
-    });
+//   const [usuario, setUsuario] = useState({
+//     login: "",
+//     avatar_url:""
+//   });
+
+const [usuarios, setUsuarios] = useState([{
+  login: "",
+  avatar_url:""
+}]);
     
+
+    useEffect(()=>{
+
+      
       //Realizando uma requisição para uma API-REST(api do github) com fetch
-      fetch('https://api.github.com/users/alecarlosjesus')
-      .then(response =>{
-          if(!response.ok){
-              throw new Error(`Erro: ${response.status}`);
-            }
-        return response.json();
-        })
-        .then(data => {
-          console.log(data);
-          setUsuario(data);
-        })
-        .catch(error => console.error("Erro ao buscar usuário : ", error));
+    async function carregarUsuarios(){
+      try{ 
+        const response = await fetch('https://api.github.com/users');
+        if(!response.ok){
+          throw new Error("Ocorreu um erro ao receber os dados");
+        }
+        const dados = await response.json();
+        setUsuarios(dados);
+      }catch(err){
+        console.log(err);
+      }
+    }
+
+  carregarUsuarios();
+
+    
+      
+  },[]);
+
       
     return(
       <div>
         <h1>Bem Vindo!</h1>
 
-          <h2>{usuario.login}</h2>
 
-          <figure>
-            <img src={usuario.avatar_url} alt="Imagem do Usuário"/>
-          </figure>
-
+        { usuarios.map((usuario)=> (
+          <div key={usuario.login}>
+            <h2>{usuario.login}</h2>
+            <img src={usuario.avatar_url} alt={usuario.login} />
+          </div>
+        ))}
 
       </div>
     );
