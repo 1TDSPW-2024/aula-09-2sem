50c35fc7eed4509b57b761232fc50259898a733c
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-05 21:02:34 -0300

    Realizando fetch com async e await

--- aula09/src/routes/Usuarios/index.tsx
+++ aula09/src/routes/Usuarios/index.tsx
@@ -1,49 +1,49 @@
 import { useEffect, useState } from "react";
 
-export default function Usuarios(){
+export default function Usuarios() {
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "Usuarios";
+  // const [usuario, setUsuario] = useState({
+  //   login: "",
+  //   avatar_url:""
+  // });
 
-    //MUDANDO O TÍTULO DA PÁGINA!!!
-    document.title = "Usuarios";
-    // const [usuario, setUsuario] = useState({
-    //   login: "",
-    //   avatar_url:""
-    // });
-    
-    const [usuarios, setUsuarios] = useState( [ {
+  const [usuarios, setUsuarios] = useState([
+    {
       login: "",
-      avatar_url:""
-    }]);
-    
+      avatar_url: "",
+    },
+  ]);
 
+  useEffect(() => {
+    //Criando uma requisição asincrona para a api do githuh com async e await com função anonima.
+    async function carregarUsuarios() {
+      try {
+        const response = await fetch("https://api.github.com/users");
+        if (!response.ok) {
+          throw new Error("Ocorreu um erro ao receber os dados");
+        }
+        const dados = await response.json();
+        setUsuarios(dados);
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
-    useEffect(()=>{
-      
-      //Realizando uma requisição para uma API-REST(api do github) com fetch
-      fetch('https://api.github.com/users')
-      .then(response =>{
-          if(!response.ok){
-              throw new Error(`Erro: ${response.status}`);
-            }
-        return response.json();
-        })
-        .then(data => {
-          console.log(data);
-          setUsuarios(data);
-        })
-        .catch(error => console.error("Erro ao buscar usuário : ", error));
-    },[]);
-      
-    return(
-      <div>
-        <h1>Bem Vindo!</h1>
+    carregarUsuarios();
 
-        { usuarios.map((usuario)=>(
-          <div key={usuario.login}>
-            <h2>{usuario.login}</h2>
-            <img src={usuario.avatar_url} alt={usuario.login} />
-          </div>
-        ))}
+  }, []);
 
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Bem Vindo!</h1>
+
+      {usuarios.map((usuario) => (
+        <div key={usuario.login}>
+          <h2>{usuario.login}</h2>
+          <img src={usuario.avatar_url} alt={usuario.login} />
+        </div>
+      ))}
+    </div>
+  );
+}
